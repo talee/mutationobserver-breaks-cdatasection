08920e43372e43b0a682d17de970ee0441231f1f
Thomas Lee <tleehoc@gmail.com>
2016-08-22 19:08:15 -0700

    Allow testing with MutationEvents.

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ Using the `observe` method of a MutationObserver in Internet Explorer 11 breaks 
 
 The CDATASection's data property should be `hello-world`, but is truncated around the hypen and only `hello` is returned.
 
-See [test.js](test.js).
+Demo page: https://talee.github.io/mutationobserver-breaks-cdatasection/. See [test.js](test.js).
+
+Workaround is to use MutationEvents, which is deprecated.
--- test.js
+++ test.js
@@ -1,17 +1,26 @@
-function Test() {
+function Test(useMutationEvents) {
   var statusOut = document.getElementById('status');
   var resultOut = document.getElementById('result');
 
-  document.registerElement('x-yolo');
-
-  var parser = new DOMParser();
-  var expectedContent = 'hello-world';
-  var result = parser.parseFromString('<div><![CDATA[' + expectedContent + ']]></div>','text/xml').firstChild.firstChild.data;
-  if (result != expectedContent) {
-    statusOut.textContent = 'FAIL';
-  } else {
-    statusOut.textContent = 'SUCCESS';
+  if (useMutationEvents) {
+    window.MutationObserver = null;
   }
-  resultOut.textContent = 'Result: "' + result + '"';
+  var registerElementPolyfill = document.createElement('script');
+  registerElementPolyfill.src = 'document-register-element.max.js';
+  registerElementPolyfill.onload = window.setTimeout(ParseTest, 500);
+  document.head.appendChild(registerElementPolyfill)
+
+  function ParseTest() {
+    document.registerElement('x-yolo');
+
+    var parser = new DOMParser();
+    var expectedContent = 'hello-world';
+    var result = parser.parseFromString('<div><![CDATA[' + expectedContent + ']]></div>','text/xml').firstChild.firstChild.data;
+    if (result != expectedContent) {
+      statusOut.textContent = 'FAIL';
+    } else {
+      statusOut.textContent = 'SUCCESS';
+    }
+    resultOut.textContent = 'Result: "' + result + '"';
+  }
 }
-setTimeout(Test, 2000);
